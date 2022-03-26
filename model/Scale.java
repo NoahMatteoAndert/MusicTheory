1b2a5bc5febb78615d942dabc40f86d4b3e69b07
NoahMatteoAndert <noah.andert06@gmx.at>
2022-03-26 15:19:42 +0000

    Created model clases for Musical Theory Conecpts

---
+++ model/Scale.java
@@ -0,0 +1,13 @@
+package model;
+public class Scale {
+    private Note rootNote;
+    private Quality quality;
+    private Chord[] diatonicChords;
+
+    public Scale(Quality scaleQuality, Note scaleRootNote){
+        
+    }
+
+    
+
+}
